a27a87f7d90199a1e88459a7f25a3426ff1177e8
MissKittyLover <mariamghlonti8@gmail.com>
2025-10-17 20:16:03 +0400

    3rd commit

---
+++ Day 1/Untitled-1.py
@@ -0,0 +1,60 @@
+#Numeric types
+#Integer(int)
+a = 9
+print(a)
+#Floating-point(float)
+b = 2.9
+print(b)
+#Complex numbers(complex)
+c = 3+9j
+print(c)
+print("")
+#Text type
+#String(str)
+Name = "Andrei Nikto"
+print(Name)
+print("")
+#Sequence types
+#List(list)
+List = [1, 2, "Simon", "Andrei"]
+print(list)
+#Tuple(tuple)
+Tuple = (1, 2, "Simon", "Ghost", "Riley")
+print(Tuple)
+#Range(range)
+rng = range(9)
+print(rng)
+print("")
+#Mappig type
+#Dictionary(dict)
+Dict = {Name: "Andrei Nikto", "Age": 29, "Organization": "KorTac"}
+print(Dict)
+print("")
+#Set Types
+#Set(set)
+SET = {"Andrei", "Simon", "Krueger"}
+print(SET)
+#Frozen set(frozenset)
+FSET = frozenset({"Disfigured", "Beautiful", "Deadly" })
+print(FSET)
+print("")
+#Bolean type
+#Bolean(bool)
+B = False
+print(B)
+print("")
+#Binary types
+#Bytes(bytes)
+BYT = b"Good evening"
+print(BYT)
+#Byte array(bytearray)
+BYTA = bytearray(29)
+print(BYTA)
+#Memory view(memoryview)
+MV = memoryview(bytearray(29))
+print(MV)
+print("")
+#None Type
+#None(NoneType)
+N = None
+print(N)
